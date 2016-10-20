f96640a244ca22e4bce0de52bad4fcc28cd94062
sdfsds5271463 <sdfsds5271463@gmail.com>
2016-10-20 22:47:39 +0800

    首頁製作版11.0 (筆記整理)
    Signed-off-by: sdfsds5271463 <sdfsds5271463@gmail.com>

--- app/Http/Controllers/test.php
+++ app/Http/Controllers/test.php
@@ -16,29 +16,11 @@ use App\Http\Controllers\test\putVarFile;
 			\App\Services\manageVarFile $mngFile,
 			Request $request
 		){
-			echo "test";
+			echo "test";        	
+		}
 
-			//$RR = ["A"=>1,"B"=>2,"C"=>3];
-			//$mngFile->putVar("TT",$RR);
-
-			print_r($mngFile->getVar("eee"));
-			//$filePath = "public/files/";
-
-        	//serialize($A); //儲存格式轉換
-        	//unserialize($A); //儲存格式還原，如實儲存php變數的方法
-			//$fileName = "usersAndTime";
-			//file_get_contents("test.txt"); //回傳檔案內容 (可以抓網址的資料)
-
-			//putVarFile("ttt",$filePath);
-			//putVarFile("ttt",$filePath);
-			
-			//$filePath = "public/files/";
-
-
-				$flag = $mngFile->getVar("chatTimeFlag");
-				$chatTimeFlag = $flag; //新版 使用檔案做為時間旗標
-				print_r($flag->flag);
-        	
+		public function getIndex(){
+			echo "getIndex";        	
 		}
 	}
 
